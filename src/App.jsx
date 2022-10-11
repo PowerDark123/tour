2ccf9e8920045f70f466fcb585bcc0c9de5c1a2d
Your Name <you@example.com>
2022-10-11 13:24:54 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {Tours} from './components/Tours'
+import './App.css';
+import { QueryClient, QueryClientProvider} from 'react-query';
+const queryClient = new QueryClient()
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Tours/>
+    </QueryClientProvider>
+    
+  );
+}
+
+export default App;
