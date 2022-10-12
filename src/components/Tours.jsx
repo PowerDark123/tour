f334c7561b7855ebd6e1e31790a7c4b8869e6c51
Your Name <you@example.com>
2022-10-12 12:53:45 +0200

    second

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import {useQuery } from 'react-query';
 import axios from 'axios';
+import {Tour} from './Tour';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
 
 const getData=async ()=>{
     const response=await axios.get('https://course-api.com/react-tours-project')
@@ -11,10 +17,20 @@ export const Tours=()=> {
     const {data,status}=useQuery('tours',getData)
 
   return (
-    <div>
+    <Box sx={{ width: '100%' }}>
+      <Typography gutterBottom display="flex" justifyContent="center" variant="h3" sx={{textDecoration:"underline",color:"#1976d2"}}>
+        Our Tours
+      </Typography>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         {status=='error' && <div>error fetching data...</div>}
         {status=='loading' && <div>loading</div>}
-        {status=='success' && <div>ok</div>}
-    </div>
+        {status=='success' && data.data.map(obj=>(
+        <Grid item xs={12} sm={6} md={3} display="flex" justifyContent="center">
+        <Tour key={obj.id} {...obj}/>
+        </Grid>
+        ))}
+      </Grid>
+        
+    </Box>
   )
 }
